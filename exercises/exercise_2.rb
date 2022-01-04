ca8c9ed7a504f536031f792cba60d7181515433f
charit nanduri <charithnanduri03@gmail.com>
2022-01-04 19:00:53 +0000

    add exercise 2 code

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,16 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+p @store1
+
+@store2 = Store.find_by(id: 2)
+p @store2
+
+# @store1.name = "Hamilton"
+# @store1.save
+# p @store1
+
+@store1.update(name:"Vancouver")
+p @store1
